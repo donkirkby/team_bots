cea5f9d2ff494c4a370087de5f18e7486334a224
AngMcL <angela.mclaugh@gmail.com>
2018-05-15 12:54:51 -0700

    use attacks

--- robot1.py
+++ robot1.py
@@ -3,9 +3,17 @@ from rgkit import rg
 
 class Robot:
     def __init__(self):
-        self.target = (10, 10)
+        self.target = (7, 7)
 
     def act(self, game):
-        if self.location == self.target:
+    	myx, myy = self.location
+    	for dx in (-1, 0, 1):
+    		for dy in (-1, 0, 1):
+    			x = myx + dx
+    			y = myy + dy
+    			target = game.robots.get((x, y), None)
+    			if target and target.player_id != self.player_id:
+    				return ['attack', (x, y)]
+    	if self.location == self.target:
             return ['guard']
         return ['move', rg.toward(self.location, self.target)]
