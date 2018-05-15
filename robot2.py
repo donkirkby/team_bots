fa5424caea2fcc16cf4b684574e8f450a108dc0d
Nathaniel Knight <nathaniel.ep@gmail.com>
2018-05-15 13:06:07 -0700

    Robot2 random strategy

--- robot2.py
+++ robot2.py
@@ -7,18 +7,33 @@ class Robot:
     def __init__(self):
         self.target = (13, 13)
 
+
     possible_moves = [
-        ["move", (-1, 1)],
-        ["move", (-1, -1)],
-        ["move", (1, 1)],
-        ["move", (1, -1)],
+        ["move", (0, 1)],
+        ["move", (0, -1)],
+        ["move", (1, 0)],
+        ["move", (-1, 0)],
         ["guard"],
+        ["attack", (1, 0)],
+        ["attack", (-1, 0)],
+        ["attack", (0, -1)],
         ["attack", (-1, 1)],
-        ["attack", (-1, -1)],
-        ["attack", (1, 1)],
-        ["attack", (1, -1)],
-        ["suicide"],
     ]
 
     def act(self, game):
-        return random.sample(self.possible_moves, 1)
+        move = random.sample(self.possible_moves, 1)[0]
+
+        def add(xy, dxdy):
+            dx, dy = dxdy
+            x, y = xy
+            return (x + dx, y + dy)
+
+        if move[0] =="move":
+            new = add(self.location, move[1])
+            direction = rg.toward(self.location, new)
+            move[1] = direction
+        if move[0] == "attack":
+            pos = add(self.location, move[1])
+            move[1] = pos
+        return move
+
