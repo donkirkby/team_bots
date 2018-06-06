a01c35a88cb48e3ebec561605833c7a4a4878f4d
Don Kirkby <donkirkby@gmail.com>
2018-06-06 13:06:30 -0700

    Robot 2 takes a swing at a nearby enemy.

--- robot2.py
+++ robot2.py
@@ -19,50 +19,61 @@ class Robot(object):
                 self.RALLY_POINT = x, y
                 break
 
+    # POSSIBLE_MOVES = [
+    #     ("move",),
+    #     ("move",),
+    #     ("move",),
+    #     ("move",),
+    #     ("move",),
+    #     ("guard",),
+    #     ("attack", (1, 0)),
+    #     ("attack", (-1, 0)),
+    #     ("attack", (0, -1)),
+    #     ("attack", (0, 1)),
+    # ]
+
     POSSIBLE_MOVES = [
-        ("move",),
-        ("move",),
-        ("move",),
-        ("move",),
-        ("move",),
-        ("guard",),
-        ("attack", (1, 0)),
-        ("attack", (-1, 0)),
-        ("attack", (0, -1)),
-        ("attack", (0, 1)),
-    ]
-
-    def choose_move(self):
-        move = random.sample(self.POSSIBLE_MOVES, 1)[0]
-        return move
-
-
-    possible_moves = [
-        ["move", (0, 1)],
-        ["move", (0, -1)],
-        ["move", (1, 0)],
-        ["move", (-1, 0)],
-        ["guard"],
-        ["attack", (1, 0)],
-        ["attack", (-1, 0)],
-        ["attack", (0, -1)],
-        ["attack", (-1, 1)],
+        "move",
+        "move",
+        "move",
+        "move",
+        "guard",
     ]
 
     def act(self, game):
-        move = self.choose_move()
+        for bot_location, robot_info in game.robots.items():
+            if robot_info.player_id == self.player_id:
+                continue
+            # Having reached here, we know this is an opposition robot.
+            dx = bot_location[0] - self.location[0]
+            dy = bot_location[1] - self.location[1]
 
-        def add(xy, dxdy):
-            dx, dy = dxdy
-            x, y = xy
-            return (x + dx, y + dy)
+            if abs(dx) + abs(dy) == 1:
+                # The opponent bot is adjacent to us -- attack!
+                return ["attack", (self.location[0] + dx, self.location[1] + dy)]
 
-        if move[0] == "move":
+        # Having reached *here*, we know that there's nothing right to us, so pick a move at random.
+        move = random.sample(self.POSSIBLE_MOVES, 1)[0]
+        if move == "move":
             direction = rg.toward(self.location, self.RALLY_POINT)
             return ["move", direction]
-        if move[0] == "attack":
-            # import pdb; pdb.set_trace()
-            pos = add(self.location, move[1])
-            return ["attack", pos]
-        return move
+        else:
+            return ["guard"]
+
+    # def act(self, game):
+    #     move = self.choose_move(game)
+    #
+    #     def add(xy, dxdy):
+    #         dx, dy = dxdy
+    #         x, y = xy
+    #         return (x + dx, y + dy)
+    #
+    #     if move[0] == "move":
+    #         direction = rg.toward(self.location, self.RALLY_POINT)
+    #         return ["move", direction]
+    #     if move[0] == "attack":
+    #         # import pdb; pdb.set_trace()
+    #         pos = add(self.location, move[1])
+    #         return ["attack", pos]
+    #     return move
 
